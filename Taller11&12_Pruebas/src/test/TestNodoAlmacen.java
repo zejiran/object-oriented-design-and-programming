b882dee0cd2fd7a8f56ba80b52e30faa1223577d
Juan Alegría <juanszalegria@gmail.com>
2021-05-30 23:22:18 -0500

    Feat[W11]: add Categoria test

--- Taller11&12_Pruebas/src/test/TestNodoAlmacen.java
+++ Taller11&12_Pruebas/src/test/TestNodoAlmacen.java
@@ -1,14 +1,3 @@
-/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Universidad  de  los Andes   (Bogot� - Colombia)
- * Departamento de  Ingenier�a  de  Sistemas    y   Computaci�n
- * Licenciado   bajo    el  esquema Academic Free License versi�n 2.1
- *      
- * Proyecto Cupi2   (http://cupi2.uniandes.edu.co)
- * Ejercicio: n11_almacen
- * Autor: Equipo Cupi2 2018
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- */
-
 package test;
 
 import java.io.BufferedReader;
@@ -19,150 +8,149 @@ import java.util.List;
 /**
  * Clase abstracta que representa un nodo del �rbol del mercado.
  */
-public abstract class TestNodoAlmacen
-{
+public abstract class TestNodoAlmacen {
 
-    // -----------------------------------------------------------------
-    // Atributos
-    // -----------------------------------------------------------------
+	// -----------------------------------------------------------------
+	// Atributos
+	// -----------------------------------------------------------------
 
-    /**
-     * Tipo del nodo.
-     */
-    protected String tipo;
+	/**
+	 * Tipo del nodo.
+	 */
+	protected String tipo;
 
-    /**
-     * Identificador del nodo.
-     */
-    protected String identificador;
-    
-    /**
-     * El nombre del nodo.
-     */
-    protected String nombre;
+	/**
+	 * Identificador del nodo.
+	 */
+	protected String identificador;
 
-    // -----------------------------------------------------------------
-    // Constructores
-    // -----------------------------------------------------------------
+	/**
+	 * El nombre del nodo.
+	 */
+	protected String nombre;
 
-    /**
-     * Crea un nuevo nodo.<br>
-     * <b>post: </b> Los atributos identificador y tipo se inicializaron con los valores dados por par�metro.
-     * @param pTipo Tipo del nodo. pTipo != null && pTipo != "".
-     * @param pIdentificador Identificador del nodo. pIdentificador != null && pIdentificador != "".
-     * @param pNombre Nombre del nodo. pNombre != null && pNombre != "".
-     */
-    public TestNodoAlmacen( String pTipo, String pIdentificador, String pNombre )
-    {
-        tipo = pTipo;
-        identificador = pIdentificador;
-        nombre = pNombre;
-    }
+	// -----------------------------------------------------------------
+	// Constructores
+	// -----------------------------------------------------------------
 
-    // -----------------------------------------------------------------
-    // M�todos
-    // -----------------------------------------------------------------
+	/**
+	 * Crea un nuevo nodo.<br>
+	 * <b>post: </b> Los atributos identificador y tipo se inicializaron con los
+	 * valores dados por par�metro.
+	 * 
+	 * @param pTipo          Tipo del nodo. pTipo != null && pTipo != "".
+	 * @param pIdentificador Identificador del nodo. pIdentificador != null &&
+	 *                       pIdentificador != "".
+	 * @param pNombre        Nombre del nodo. pNombre != null && pNombre != "".
+	 */
+	public TestNodoAlmacen(String pTipo, String pIdentificador, String pNombre) {
+		tipo = pTipo;
+		identificador = pIdentificador;
+		nombre = pNombre;
+	}
 
-    /**
-     * Retorna el identificador del nodo.
-     * @return Identificador del nodo.
-     */
-    public String darIdentificador( )
-    {
-        return identificador;
-    }
+	// -----------------------------------------------------------------
+	// M�todos
+	// -----------------------------------------------------------------
 
-    /**
-     * Retorna el tipo del nodo.
-     * @return Tipo del nodo.
-     */
-    public String darTipo( )
-    {
-        return tipo;
-    }
+	/**
+	 * Retorna el identificador del nodo.
+	 * 
+	 * @return Identificador del nodo.
+	 */
+	public String darIdentificador() {
+		return identificador;
+	}
 
-    /**
-     * Retorna el nombre del nodo.
-     * @return Nombre del nodo.
-     */
-    public String darNombre(){
-        return nombre;
-    }
-    
-    /**
-     * Busca el nodo con el identificador dado.
-     * @param pIdentificador Identificador del nodo. pIdentificador != null && pIdentificador != "".
-     * @return NodoAlmacen con el identificador dado o null si no se encontr� el nodo.
-     */
-    public abstract TestNodoAlmacen buscarNodo( String pIdentificador );
+	/**
+	 * Retorna el tipo del nodo.
+	 * 
+	 * @return Tipo del nodo.
+	 */
+	public String darTipo() {
+		return tipo;
+	}
 
-    /**
-     * Retorna el valor total de las ventas del nodo.
-     * @return Valor ventas del nodo.
-     */
-    public abstract double darValorVentas( );
+	/**
+	 * Retorna el nombre del nodo.
+	 * 
+	 * @return Nombre del nodo.
+	 */
+	public String darNombre() {
+		return nombre;
+	}
 
-    /**
-     * Crea un nodo a partir de la informaci�n del lector.
-     * @param pLector Lector que contiene la informaci�n. pLector != null.
-     * @return NodoAlmacen creado.
-     * @throws TestAlmacenException Si ocurre alg�n error al leer la informaci�n.
-     */
-    public TestNodoAlmacen crearNodo( BufferedReader pLector ) throws TestAlmacenException
-    {
-        try
-        {
-            String linea = pLector.readLine( );
-            String datos[] = linea.split( ";;;" );
-            TestNodoAlmacen respuesta = null;
-            if( datos[ 0 ].equals( TestCategoria.TIPO ) )
-            {
-                respuesta = new TestCategoria( linea, pLector );
-            }
-            else if( datos[ 0 ].equals( TestMarca.TIPO ) )
-            {
-                respuesta = new TestMarca( linea, pLector );
-            }
-            else
-            {
-                throw new TestAlmacenException( datos[ 0 ] + " no es un tipo de nodo valido." );
-            }
-            return respuesta;
+	/**
+	 * Busca el nodo con el identificador dado.
+	 * 
+	 * @param pIdentificador Identificador del nodo. pIdentificador != null &&
+	 *                       pIdentificador != "".
+	 * @return NodoAlmacen con el identificador dado o null si no se encontr� el
+	 *         nodo.
+	 */
+	public abstract TestNodoAlmacen buscarNodo(String pIdentificador);
 
-        }
-        catch( IOException e )
-        {
-            throw new TestAlmacenException( "Error al leer el archivo.\n" + e.getMessage( ) );
-        }
-    }
+	/**
+	 * Retorna el valor total de las ventas del nodo.
+	 * 
+	 * @return Valor ventas del nodo.
+	 */
+	public abstract double darValorVentas();
 
-    /**
-     * Agrega a la lista acumulada todos los productos del nodo.<br>
-     * <b>pre:</b> La lista de nodos est� inicializada.
-     * @param pProductos Lista acumulada con los productos. pProductos != null.
-     */
-    public abstract void darProductos( List<TestProducto> pProductos );
+	/**
+	 * Crea un nodo a partir de la informaci�n del lector.
+	 * 
+	 * @param pLector Lector que contiene la informaci�n. pLector != null.
+	 * @return NodoAlmacen creado.
+	 * @throws TestAlmacenException Si ocurre alg�n error al leer la informaci�n.
+	 */
+	public TestNodoAlmacen crearNodo(BufferedReader pLector) throws TestAlmacenException {
+		try {
+			String linea = pLector.readLine();
+			String datos[] = linea.split(";;;");
+			TestNodoAlmacen respuesta = null;
+			if (datos[0].equals(TestCategoria.TIPO)) {
+				respuesta = new TestCategoria(linea, pLector);
+			} else if (datos[0].equals(TestMarca.TIPO)) {
+				respuesta = new TestMarca(linea, pLector);
+			} else {
+				throw new TestAlmacenException(datos[0] + " no es un tipo de nodo valido.");
+			}
+			return respuesta;
 
-    /**
-     * Retorna una lista con todos los productos de la categor�a.<br>
-     * <b>pre:</b> La lista de nodos est� inicializada.
-     * @return Lista con los productos del nodo.
-     */
-    public List<TestProducto> darProductos( )
-    {
-        List<TestProducto> productos = new ArrayList<>( );
-        darProductos( productos );
-        return productos;
-    }
-    
-    /**
-     * Retorna la representaci�n en string de este objeto.<br>
-     * @return Nombre del nodo.
-     */
-    @Override
-    public String toString( )
-    {
-        return nombre;
-    }
+		} catch (IOException e) {
+			throw new TestAlmacenException("Error al leer el archivo.\n" + e.getMessage());
+		}
+	}
+
+	/**
+	 * Agrega a la lista acumulada todos los productos del nodo.<br>
+	 * <b>pre:</b> La lista de nodos est� inicializada.
+	 * 
+	 * @param pProductos Lista acumulada con los productos. pProductos != null.
+	 */
+	public abstract void darProductos(List<TestProducto> pProductos);
+
+	/**
+	 * Retorna una lista con todos los productos de la categor�a.<br>
+	 * <b>pre:</b> La lista de nodos est� inicializada.
+	 * 
+	 * @return Lista con los productos del nodo.
+	 */
+	public List<TestProducto> darProductos() {
+		List<TestProducto> productos = new ArrayList<>();
+		darProductos(productos);
+		return productos;
+	}
+
+	/**
+	 * Retorna la representaci�n en string de este objeto.<br>
+	 * 
+	 * @return Nombre del nodo.
+	 */
+	@Override
+	public String toString() {
+		return nombre;
+	}
 
 }
